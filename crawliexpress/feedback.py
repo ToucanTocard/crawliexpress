3c1cf1f7658c4353d1f19805eed0461606d72f7b
Robin Kleinermanns <contact@robin.ninja>
2020-10-09 12:23:02 +0200

    fixed bs4 usage

--- crawliexpress/feedback.py
+++ crawliexpress/feedback.py
@@ -29,7 +29,7 @@ class Feedback:
     def from_node(self, node):
 
         # user name
-        node_user = node.find("", class_="user-name")
+        node_user = node.find(class_="user-name")
         if node_user is None:
             raise CrawliexpressException("could not find feedback user node")
         self.user = node_user.text.strip()
@@ -41,13 +41,13 @@ class Feedback:
             self.profile = node_profile["href"]
 
         # country
-        node_country = node.find("", class_="user-country")
+        node_country = node.find(class_="user-country")
         if node_country is None:
             raise CrawliexpressException("could not find feedback country node")
         self.country = node_country.text
 
         # rating
-        node_rating = node.find("", class_="star-view")
+        node_rating = node.find(class_="star-view")
         if node_rating is None:
             raise CrawliexpressException("could not find feedback rating node")
         for node_rating_stars in node_rating.children:
@@ -56,16 +56,16 @@ class Feedback:
                 self.rating = int(rating_matches.group(1))
 
         # review part
-        node_review = node.find("", class_="buyer-review")
+        node_review = node.find(class_="buyer-review")
         if node_review is None:
             raise CrawliexpressException("could not find feedback review node")
 
-        node_comment = node_review.find("", class_="buyer-feedback")
+        node_comment = node_review.find(class_="buyer-feedback")
         if node_comment is None:
             raise CrawliexpressException("could not find feedback comment node")
 
         # datetime
-        node_datetime = node_comment.find("", class_="r-time-new")
+        node_datetime = node_comment.find(class_="r-time-new")
         if node_datetime is None:
             raise CrawliexpressException("could not find feedback datetime")
         self.datetime = node_datetime.text
@@ -78,7 +78,7 @@ class Feedback:
 
         # images
         images = self.images = list()
-        node_images = node.find("", class_="r-photo-list")
+        node_images = node.find(class_="r-photo-list")
         if node_images is not None:
             for node_image in node_images.find_all("img"):
                 images.append(node_image["src"])
--- crawliexpress/feedback_page.py
+++ crawliexpress/feedback_page.py
@@ -23,17 +23,17 @@ class FeedbackPage:
 
         # feedbacks
         feedbacks = self.feedbacks = list()
-        for node in soup.find_all("", class_="feedback-item"):
+        for node in soup.find_all(class_="feedback-item"):
             feedback = Feedback()
             feedback.from_node(node)
             feedbacks.append(feedback)
 
         # page
-        node_pages = soup.find("", class_="ui-pagination-navi")
+        node_pages = soup.find(class_="ui-pagination-navi")
         if node_pages is None:
             raise CrawliexpressException("could not find pagination node")
 
-        node_page = node_pages.find("", class_="ui-pagination-active")
+        node_page = node_pages.find(class_="ui-pagination-active")
         if node_page is None:
             raise CrawliexpressException("could not find current page")
         page = node_page.text
@@ -43,7 +43,7 @@ class FeedbackPage:
 
         # pages
         known_pages = self.known_pages = list()
-        for node_page in node_pages.find_all("", class_="ui-goto-page"):
+        for node_page in node_pages.find_all(class_="ui-goto-page"):
             page = node_page.text
             if re.match(r"^\d+$", page) is not None:
                 known_pages.append(int(page))
